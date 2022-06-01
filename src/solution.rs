83c21ea4470bf6afcfe0ebdb09cf41aa13bb20c0
Eder Santos <3derms@gmail.com>
2022-06-01 19:30:19 -0300

    feat: resolve parcialmente case 4

--- src/solution.rs
+++ src/solution.rs
@@ -5,10 +5,25 @@ type Inventory<'a> = Vec<(i32, &'a str)>;
 /// Don't worry about the `'a` syntax, this is a sligtly advanced concept in Rust called Lifetimes
 /// but you don't need to deeple understand this in order to complete this challenge.
 /// You can learn more about lifetimes [here](https://doc.rust-lang.org/rust-by-example/scope/lifetime.html).
-pub fn update_inventory<'a>(cur_inv: Inventory<'a>, new_inv: Inventory<'a>) -> Inventory<'a> {
+pub fn update_inventory<'a>(
+    mut cur_inv: Inventory<'a>,
+    mut new_inv: Inventory<'a>,
+) -> Inventory<'a> {
     if cur_inv.len() == 0 && new_inv != cur_inv {
         return new_inv;
     }
-
+    if cur_inv.len() != 0 && new_inv.len() != 0 {
+        for j in 0..new_inv.len() {
+            let (new_value, new_name) = new_inv[j];
+            let mut count = 0;
+            for i in 0..cur_inv.len() {
+                let (cur_value, cur_name) = cur_inv[i];
+                if cur_name == new_name {
+                    let new_val = cur_value + new_value;
+                    cur_inv[i] = (new_val, new_name);
+                }
+            }
+        }
+    }
     return cur_inv;
 }
