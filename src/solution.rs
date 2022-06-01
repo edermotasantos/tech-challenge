51305ce2f72269dcb8200fc80256738e4bb4a86f
Eder Santos <3derms@gmail.com>
2022-06-01 19:26:40 -0300

    feat: resolve case 1 e 3

--- src/solution.rs
+++ src/solution.rs
@@ -6,6 +6,9 @@ type Inventory<'a> = Vec<(i32, &'a str)>;
 /// but you don't need to deeple understand this in order to complete this challenge.
 /// You can learn more about lifetimes [here](https://doc.rust-lang.org/rust-by-example/scope/lifetime.html).
 pub fn update_inventory<'a>(cur_inv: Inventory<'a>, new_inv: Inventory<'a>) -> Inventory<'a> {
-    todo!()
-    /// primeiro commit 
+    if cur_inv.len() == 0 && new_inv != cur_inv {
+        return new_inv;
+    }
+
+    return cur_inv;
 }
