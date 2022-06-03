df1725d1186aaea06f0bed2b9d2a8b8225495397
Eder Santos <3derms@gmail.com>
2022-06-03 11:31:44 -0300

    refactor: diminui complexidade de algoritmo com HashMap

--- src/solution.rs
+++ src/solution.rs
@@ -1,6 +1,7 @@
 /// In Rust, the `type` keyword is used to define an alias for an existing type
 /// If you want, you can learn more about it [here](https://doc.rust-lang.org/std/keyword.type.html)
 type Inventory<'a> = Vec<(i32, &'a str)>;
+use std::collections::HashMap;
 
 /// Don't worry about the `'a` syntax, this is a sligtly advanced concept in Rust called Lifetimes
 /// but you don't need to deeple understand this in order to complete this challenge.
@@ -13,21 +14,20 @@ pub fn update_inventory<'a>(
     if cur_inv.len() == 0 {
         return new_inv;
     }
-    for index_i in 0..new_inv.len() {
-        let (new_value, name) = new_inv[index_i];
-        let mut count = 0;
-        for index_j in 0..cur_inv.len() {
-            let (cur_value, cur_name) = cur_inv[index_j];
-            if cur_name == name {
-                let new_val = cur_value + new_value;
-                cur_inv[index_j] = (new_val, name);
+    let mut map = HashMap::new();
+    for index_i in 0..cur_inv.len() {
+        let (cur_value, cur_name) = cur_inv[index_i];
+        map.insert(cur_name, cur_value);
+    }
+    for index_j in 0..new_inv.len() {
+        let (new_value, name) = new_inv[index_j];
+        match map.get(name) {
+            Some(val) => {
+                let new_val = val + new_value;
+                let index_element = cur_inv.iter().position(|&x| x == (*val, name)).unwrap();
+                cur_inv[index_element] = (new_val, name);
             }
-            if cur_name != name {
-                count += 1;
-            }
-            if count == cur_inv.len() {
-                cur_inv.push(new_inv[index_i]);
-            }
+            None => cur_inv.push(new_inv[index_j]),
         }
     }
     cur_inv.sort_by_key(|key| key.1);
