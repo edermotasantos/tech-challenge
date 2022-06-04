b88910d94ee60b7e3cf9c6b0cc712b04c4f98154
Eder Santos <3derms@gmail.com>
2022-06-04 17:47:07 -0300

    refactor: altera espaçamento

--- src/solution.rs
+++ src/solution.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
+
 /// In Rust, the `type` keyword is used to define an alias for an existing type
 /// If you want, you can learn more about it [here](https://doc.rust-lang.org/std/keyword.type.html)
 type Inventory<'a> = Vec<(i32, &'a str)>;
-use std::collections::HashMap;
 
 /// Don't worry about the `'a` syntax, this is a sligtly advanced concept in Rust called Lifetimes
 /// but you don't need to deeple understand this in order to complete this challenge.
@@ -11,14 +12,17 @@ pub fn update_inventory<'a>(
     mut new_inv: Inventory<'a>,
 ) -> Inventory<'a> {
     new_inv.sort_by_key(|key| key.1);
+
     if cur_inv.len() == 0 {
         return new_inv;
     }
+
     let mut map = HashMap::new();
     for index_i in 0..cur_inv.len() {
         let (cur_value, cur_name) = cur_inv[index_i];
         map.insert(cur_name, cur_value);
     }
+
     for index_j in 0..new_inv.len() {
         let (new_value, name) = new_inv[index_j];
         match map.get(name) {
@@ -31,5 +35,6 @@ pub fn update_inventory<'a>(
         }
     }
     cur_inv.sort_by_key(|key| key.1);
+
     return cur_inv;
 }
